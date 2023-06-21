e046dba87f7f5bc76611b3d0c0be18df31e8ce45
Henri P Gavin <henri.gavin@duke.edu>
2023-06-21 06:03:43 -0400

    adding circular buffer code for future use

---
+++ src/cirbuff.c
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2019 EmbedJournal
+ *
+ * SPDX-License-Identifier: MIT
+ */
+
+#include <string.h>
+
+#include "cirbuff.h"
+
+int __circ_gbuf_pop(circ_gbuf_t *circ_buf, void *elem, int read_only)
+{
+	int total;
+	char *tail;
+
+	total = circ_buf->push_count - circ_buf->pop_count;
+	if (total < 0)
+		total += (2 * circ_buf->size);
+
+	if (total == 0)
+		return -1; // Empty
+
+	tail = (char *)circ_buf->buffer + ((circ_buf->pop_count % circ_buf->size)
+			* circ_buf->element_size);
+
+	if (elem)
+		memcpy(elem, tail, circ_buf->element_size);
+
+	if (!read_only) {
+#ifdef CRICBUF_CLEAN_ON_POP
+		memset(tail, 0, circ_buf->element_size);
+#endif
+		circ_buf->pop_count++;
+		if (circ_buf->pop_count >= (2*circ_buf->size))
+			circ_buf->pop_count = 0;
+	}
+	return 0;
+}
+
+int __circ_gbuf_push(circ_gbuf_t *circ_buf, void *elem)
+{
+	int total;
+	char *head;
+
+	total = circ_buf->push_count - circ_buf->pop_count;
+	if (total < 0)
+		total += (2 * circ_buf->size);
+
+	if (total >=  circ_buf->size)
+		return -1; // Full
+
+	head = (char *)circ_buf->buffer + ( (circ_buf->push_count % circ_buf->size)
+			* circ_buf->element_size );
+	memcpy(head, elem, circ_buf->element_size);
+	circ_buf->push_count++;
+	if (circ_buf->push_count >= (2*circ_buf->size))
+		circ_buf->push_count = 0;
+	return 0;
+}
+
+int __circ_gbuf_free_space(circ_gbuf_t *circ_buf)
+{
+	int total;
+
+	total = circ_buf->push_count - circ_buf->pop_count;
+	if (total < 0)
+		total += (2 * circ_buf->size);
+
+	return circ_buf->size - total;
+}
---
+++ src/cirbuff.h
@@ -0,0 +1,120 @@
+/*
+ * copyright (c) 2019 embedjournal
+ *
+ * spdx-license-identifier: mit
+ */
+
+#ifndef __CIRCULAR_GENERIC_BUFFER_H_
+#define __CIRCULAR_GENERIC_BUFFER_H_
+
+#include <stdint.h>
+
+/** --- Internal method and structures. DON'T USE --------------------------- */
+typedef struct {
+	void * const buffer;
+	uint16_t push_count;
+	uint16_t pop_count;
+	uint16_t const size;
+	uint16_t const element_size;
+} circ_gbuf_t;
+
+#define __CIRC_GBUF_V_DEF(type, buf, sz)		\
+	type buf ## _circ_gbuf_data[sz];		\
+	circ_gbuf_t buf = {				\
+		.buffer = buf ## _circ_gbuf_data,	\
+		.push_count = 0,			\
+		.pop_count = 0,				\
+		.size = sz,				\
+		.element_size = sizeof(type)		\
+	};
+
+int __circ_gbuf_push(circ_gbuf_t *circ_gbuf, void *elem);
+int __circ_gbuf_pop (circ_gbuf_t *circ_gbuf, void *elem, int read_only);
+int __circ_gbuf_free_space(circ_gbuf_t *circ_gbuf);
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Description:
+ *   Zero slots in circular buffer after a pop.
+ */
+// #define CRICBUF_CLEAN_ON_POP
+
+/**
+ * Description:
+ *   Defines a global circular buffer `buf` of a given type and size. The type
+ *   can be native data types or user-defined data types.
+ *
+ * Usage:
+ *   CIRC_GBUF_DEF(uint8_t, byte_buf, 13);
+ *   CIRC_GBUF_DEF(struct foo, foo_buf, 10);
+ */
+#define CIRC_GBUF_DEF(type, buf, size)			\
+	__CIRC_GBUF_V_DEF(type, buf, size)		\
+	int buf ## _push_refd(type *pt)			\
+	{						\
+		return __circ_gbuf_push(&buf, pt);	\
+	}						\
+	int buf ## _pop_refd(type *pt)			\
+	{						\
+		return __circ_gbuf_pop(&buf, pt, 0);	\
+	}						\
+	int buf ## _peek_refd(type *pt)			\
+	{						\
+		return __circ_gbuf_pop(&buf, pt, 1);	\
+	}
+
+/**
+ * Description:
+ *   Resets the circular buffer offsets to zero. Does not clean the newly freed
+ *   slots.
+ */
+#define CIRC_GBUF_FLUSH(buf)				\
+	do {						\
+		buf.push_count = 0;			\
+		buf.pop_count = 0;			\
+	} while(0)
+
+/**
+ * Description:
+ *   Pushes element pointed to by `elem` at the head of circular buffer `buf`.
+ *   This is read-write method, occupancy count increases by one.
+ *
+ * Returns (int):
+ *   0 - Success
+ *  -1 - Out of space
+ */
+#define CIRC_GBUF_PUSH(buf, elem)             buf ## _push_refd(elem)
+
+/**
+ * Description:
+ *   Removes the element at tail from circular buffer `buf` and makes it
+ *   available at `elem`. This is read-write method, occupancy count reduces
+ *   by one.
+ *
+ * Returns (int):
+ *   0 - Success
+ *  -1 - Empty
+ */
+#define CIRC_GBUF_PEEK(buf, elem)             buf ## _peek_refd(elem)
+
+/**
+ * Description:
+ *   Copies the element at tail of circular buffer `buf` into location pointed
+ *   by `elem`. This method is read-only, does not later occupancy status.
+ *
+ * Returns (int):
+ *   0 - Success
+ *  -1 - Empty
+ */
+#define CIRC_GBUF_POP(buf, elem)              buf ## _pop_refd(elem)
+
+/**
+ * Description:
+ *   Returns the number of free slots in the circular buffer `buf`.
+ *
+ * Returns (int):
+ *   0..N - number of slots available.
+ */
+#define CIRC_GBUF_FS(buf)                     __circ_gbuf_free_space(&buf)
+
+#endif
